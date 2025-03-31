e4ac51deafc5d5824caed14f9f93c183a2fd4b6d
Aditya Gupta <adityag860libra@gmail.com>
2025-03-31 12:12:03 +0530

    Update auth.router.js
    Add token verification in reset-password endpoint

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,7 +6,7 @@ import firebaseAuthController from '../controllers/auth.controller.js';
 authRouter.post('/api/register', firebaseAuthController.registerUser);
 authRouter.post('/api/login', firebaseAuthController.loginUser);    
 authRouter.post('/api/logout', firebaseAuthController.logoutUser);
-authRouter.post('/api/reset-password', firebaseAuthController.resetPassword);
+authRouter.post('/api/reset-password', verifyToken, firebaseAuthController.resetPassword);
 
 authRouter.get('/api/protected', verifyToken, (req,res) => {
     res.json({message: "You have accessed a protected route!", user: req.user});
