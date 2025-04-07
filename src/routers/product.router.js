03c918d0e3007746b94cab15a2fd015f9c7a9093
Aditya Gupta <adityag860libra@gmail.com>
2025-04-08 01:28:36 +0530

    Update product.router.js

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from "../middleware/verify.jwt.js";
 import * as productController from "../controllers/product.controller.js";
+import { uploadFile } from "../middleware/file.upload.js";
 
 const router = express.Router();
 
-router.post("/api/products/add-product", verifyToken, productController.addProduct);
+router.post("/api/products/add-product", verifyToken, uploadFile.single("document"), productController.addProduct);
 router.get("/api/products/get-all-products", verifyToken, productController.getAllProducts);
 router.get("/api/products/get-product/:productId", verifyToken, productController.getProduct);
 router.put("/api/products/update-product/:productId", verifyToken, productController.updateProduct);
